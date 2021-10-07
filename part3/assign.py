099917540db1b082799b3a3f2e9e4ad4fd88606e
jagchau <jagchau@iu.edu>
2021-10-06 21:29:23 -0400

    sol3

--- part3/assign.py
+++ part3/assign.py
@@ -6,9 +6,56 @@
 # Based on skeleton code by D. Crandall and B551 Staff, September 2021
 #
 
+from itertools import combinations, permutations
+from os import name
 import sys
 import time
 from queue import PriorityQueue
+
+def differenceOfLists(list1, list2):
+    return (list(set(list1) - set(list2)))
+
+def fitnessFunction(team,rTeam):
+    aTeam=team.split('-')
+    fitness=0
+    for member in aTeam:
+        userMap=list(filter(lambda user:user['user']==member,rTeam))[0]
+        love=3 if any((pref not in ['xxx', 'zzz'] and pref not in aTeam) for pref in userMap['workWith']) else 0
+        hate=len([ member for member in aTeam if member in userMap['notWorkWith'] ])*10
+        sizeAnomaly = 2 if len(userMap['workWith'])==len(aTeam) else 0
+        fitness += love + hate + sizeAnomaly + (3 - len(aTeam)) * 5
+    return fitness
+
+def calculateCost(teams, rTeam):
+    # 1. grading time
+    grading = len(teams) * 5
+
+    # 2. Size mismatch time
+    totalMissedSizes = 0
+    totalMissFav = 0
+    totalDeanTime=0
+
+    for user in rTeam:
+        for team in teams:
+            if user['user'] in team:
+                thisTeam = team.split('-')
+                if len(thisTeam) != len(user['workWith']):
+                    totalMissedSizes += 1
+                # user fav team will not have user itself, and no preference is not considered
+                userFavTeam = [member for member in user['workWith']
+                               if member != user['user'] and member not in ['xxx', 'zzz']]
+                # #people with whom the user may share code. 
+                totalMissFav += len(differenceOfLists(userFavTeam,
+                                    thisTeam))
+                # #people with whom the user got assigned, against his choice
+                userHateTeam = [ member for member in thisTeam if member in user['notWorkWith'] ]
+                totalDeanTime += len(userHateTeam)
+    
+
+    return (totalDeanTime * 10 + totalMissedSizes * 2 + grading + totalMissFav * 3)
+
+
+        
 
 def comput_cost(comb, preferences,costs = {}):
         pref = preferences[comb[0]]
@@ -18,7 +65,7 @@ def comput_cost(comb, preferences,costs = {}):
         # if comb == ['fanjun', 'djcran']:
         #     print(pref_list, comb)
         
-        if any([user not in comb and user != 'xxx' for user in pref[0].split('-')]):
+        if any([user not in comb and user not in ['xxx', 'zzz'] for user in pref[0].split('-')]):
                 if comb[0] not in costs.keys():
                     costs[comb[0]] = {comb_str:3}
                 else:
@@ -64,56 +111,34 @@ def solver(input_file):
        call "yield" to return that preliminary solution. Your program can continue yielding multiple times;
        our test program will take the last answer you 'yielded' once time expired.
     """
+
     import copy
-    from itertools import permutations
-    preferences = parse_responses(input_file)
+    from itertools import combinations
+    preferences = readInputFiles(input_file)
     names_comb = []
-    fringe = PriorityQueue()
-    names = [res for res in preferences.keys()]
-    names_comb = list(permutations(names,3))+list(permutations(names,2))+list(permutations(names,1))
-    # for i in range(len(names)):
-    #     names_copy = copy.deepcopy(names)
-    #     names_copy.remove(names[i])
-    #     for j in range(len(names_copy)-1):
-    #         pref = preferences[names[i]][0].split('-')
-    #         names_comb.append([names[i],names_copy[j],names_copy[j+1]])
-    #         if len(pref)==1:
-    #            names_comb.append([names[i]])
-    #         elif len(pref)==2:
-    #             names_comb.append([names[i],names_copy[j]])
-    # print(len(names_comb))
-    costs = {}
+    names = [res.get('user') for res in preferences]
+    names_comb = list(combinations(names,3))+list(combinations(names,2))+list(combinations(names,1))
+    all_cost_combs = []
     for comb in names_comb:
-        costs = comput_cost(comb,preferences)
+        all_cost_combs.append((comb, fitnessFunction('-'.join(comb), preferences)))
+    all_cost_combs = sorted(all_cost_combs,key=lambda x:x[1])
     final_combs = []
     final_names =[]
-    all_cost_combs = []
-    all_solutions = []
-    for user in costs.keys():
-        sorted_cost_combs = list(costs[user].items())
-        all_cost_combs+=sorted_cost_combs
-    # print(all_cost_combs)
     import random
     final_cost = sys.float_info.max
     while True:
         for cost_comb in all_cost_combs:
                 lowest_cost_comb = list(cost_comb)
-                if all([name not in final_names for name in lowest_cost_comb[0].split('-')]):
-                    lowest_cost_comb_list = lowest_cost_comb[0].split('-')
-                    if len(lowest_cost_comb_list) == 3:
-                        lowest_cost_comb[1] += costs[lowest_cost_comb_list[1]][f'{lowest_cost_comb_list[1]}-{lowest_cost_comb_list[0]}-{lowest_cost_comb_list[2]}']
-                        lowest_cost_comb[1] += costs[lowest_cost_comb_list[2]][f'{lowest_cost_comb_list[2]}-{lowest_cost_comb_list[0]}-{lowest_cost_comb_list[1]}']
-                    if len(lowest_cost_comb_list) == 2:
-                        lowest_cost_comb[1] += costs[lowest_cost_comb_list[1]][f'{lowest_cost_comb_list[1]}-{lowest_cost_comb_list[0]}']
+                if not any([name in final_names for name in lowest_cost_comb[0]]):
                     final_combs.append(lowest_cost_comb)
-                    final_names += lowest_cost_comb[0].split('-')
+                    final_names += lowest_cost_comb[0]
                 if sorted(final_names)==sorted(names):
-                            current_groups = list(map(lambda x:x[0],final_combs))
-                            current_cost = sum(list(map(lambda x:x[1],final_combs)))+len(current_groups)*5
+                            current_groups = list(map(lambda x:'-'.join(x[0]),final_combs))
+                            current_cost = calculateCost(current_groups, preferences)
+                            final_names = []
+                            final_combs = []
                             if final_cost>current_cost:
                                 final_cost = current_cost
-                                final_names = []
-                                final_combs = []
                                 yield {"assigned-groups":current_groups,"total-cost":final_cost}
         random.shuffle(all_cost_combs)
             # c =0
@@ -177,6 +202,18 @@ def parse_responses(path):
                 for line in f.read().rstrip("\n").split("\n"):
                     names_dict[line.split()[0]] = line.split()[1:] 
     return names_dict
+
+def readInputFiles(input_file):
+    data = []
+    with open(input_file, 'r') as file:
+        for line in file:
+            data.append([user for user in line.split()])
+
+    def mapUserData(user):
+        workWith = user[1].split('-')
+        # workWith.remove(user[0])
+        return {'user': user[0], 'workWith': workWith, 'notWorkWith': user[2].split(',')}
+    return list(map(mapUserData, data))
 
 if __name__ == "__main__":
     if(len(sys.argv) != 2):
